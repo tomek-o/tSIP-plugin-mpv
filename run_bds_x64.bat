d4642f44ba1cf10672ef855bb79cf4c50dfefc85
Tomasz Ostrowski <tomasz.o.ostrowski@gmail.com>
2024-07-30 20:59:53 +0200

    Fix convenience batch files for Windows 7 + Turbo Explorer

--- run_bds_x64.bat
+++ run_bds_x64.bat
@@ -3,4 +3,4 @@ REM Handling weird issue with BDS2006 and Windows 7 x64
 del %temp%\EditorLineEnds*.ttr
 set dt=%date%_%time:~3,2%-%time:~6,2%
 rename "%temp%\EditorLineEnds.ttr" "EditorLineEnds_%dt%.ttr"
-start "" "C:\Program Files (x86)\Borland\BDS\4.0\Bin\bds.exe" GpioDll.bdsgroup
+start "" "C:\Program Files (x86)\Borland\BDS\4.0\Bin\bds.exe" tSIP_mpv.bdsgroup
